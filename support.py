45feadc00f7b3b5fb77134d32f5979c526c44f2a
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-04-19 16:52:12 +0200

    Add test username

--- support.py
+++ support.py
@@ -1,13 +1,19 @@
 from datetime import datetime
 
 def get_user(catcher = None):
+    '''
+    Infer the user using usernames.json
+    '''
     if catcher is None:
         raise Exception("CAUGHT!")
     
     username = None
+    username = 'TEST'
 
     if username is None:
         raise Exception("Fatal error. Who the hell is trying to use the service??!!")
+    
+    return username
 
     
 def get_current_timestamp_string():
