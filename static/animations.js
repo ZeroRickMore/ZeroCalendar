f07a6ec825e3882376eff5abfd49ea90279544d1
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-04-21 16:44:37 +0200

    Create animations.js

---
+++ static/animations.js
@@ -0,0 +1,60 @@
+// SUPPORT =====================================================================
+function sleep(s) {
+    return new Promise(resolve => setTimeout(resolve, s*1000));
+}
+
+function isEditActive(dayEntry) {
+    return dayEntry.classList.contains('isEditActive');
+}
+
+function areDetailsActive(dayEntry) {
+    return dayEntry.classList.contains('isDetailsActive');
+}
+// =============================================================================
+
+
+function toggleDetails(entry) {
+    entry = entry.closest('.day-entry')
+
+    if (isEditActive(entry)) { // If in edit mode, just handle the edit form...
+        const editForm = entry.querySelector('.edit-form');
+        editForm.classList.toggle('show');
+    } else { // If not, just handle the details...
+        const details = entry.querySelector('.day-details');   
+        details.classList.toggle('show');
+        entry.classList.toggle('isDetailsActive');
+    }
+
+    const arrow = entry.querySelector('.arrow');
+    arrow.classList.toggle('rotate');
+}
+
+async function toggleEditForm(icon) {
+    // Order of action changes
+    const entry = icon.closest('.day-entry');
+    const editForm = entry.querySelector('.edit-form');
+    const details = entry.querySelector('.day-details');   
+
+    if (!isEditActive(entry)) {  // It was not in edit mode
+        if (areDetailsActive(entry)) { // If details were shown
+            details.classList.remove('show'); // Then remove them
+            await sleep(0.3); // And wait for the animation to end
+        }
+        editForm.classList.add('show'); // Then add the edit form
+        entry.classList.add('isEditActive'); // Finally, put in edit mode
+
+    } else { // It was in edit mode
+        editForm.classList.remove('show');
+        await sleep(0.3);
+        details.classList.add('show');
+        entry.classList.remove('isEditActive') // No more edit mode
+    }
+}
+
+function goBack() {
+    window.history.back();
+}
+
+function goForward() {
+    window.history.forward();
+}
