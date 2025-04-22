574c5625b18a55d16fee47da4c8569d42a5f7692
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-04-22 16:09:22 +0200

    Only log when you are not main, seems legit

--- loggers.py
+++ loggers.py
@@ -11,22 +11,23 @@ class RequestFormatter(logging.Formatter):
         record.method = request.method if request else 'N/A'
         record.path = request.path if request else 'N/A'
         return super().format(record)
-    
-formatter = RequestFormatter(
-    '%(asctime)s - %(levelname)s - %(remote_addr)s - %(method)s %(path)s - %(message)s'
-)
 
-file_handler = logging.FileHandler('ZeroCalendar.log')
-file_handler.setFormatter(formatter)
+if __name__ != '__main__':
+    formatter = RequestFormatter(
+        '%(asctime)s - %(levelname)s - %(remote_addr)s - %(method)s %(path)s - %(message)s'
+    )
 
-# Flask has too many info logs, I don't want that many...
-werkzeug_logger = logging.getLogger('werkzeug')
-werkzeug_logger.setLevel(logging.WARNING)
-werkzeug_logger.addHandler(file_handler)
+    file_handler = logging.FileHandler('ZeroCalendar.log')
+    file_handler.setFormatter(formatter)
 
-database_logger = logging.getLogger('db')
-database_logger.setLevel(logging.INFO)
-database_logger.addHandler(file_handler)
+    # Flask has too many info logs, I don't want that many...
+    werkzeug_logger = logging.getLogger('werkzeug')
+    werkzeug_logger.setLevel(logging.WARNING)
+    werkzeug_logger.addHandler(file_handler)
 
-# This is not the correct logger for this message, but I will not create a different one just for this...
-database_logger.info("=================< SERVER JUST STARTED >=================") 
+    database_logger = logging.getLogger('db')
+    database_logger.setLevel(logging.INFO)
+    database_logger.addHandler(file_handler)
+
+    # This is not the correct logger for this message, but I will not create a different one just for this...
+    database_logger.info("=================< SERVER JUST STARTED >=================")
