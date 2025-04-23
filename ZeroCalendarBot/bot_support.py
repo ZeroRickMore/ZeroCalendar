f6d35aa09ac28f7fad04fb869de185968df27e44
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-04-23 19:40:04 +0200

    dotenv simplified functions

---
+++ ZeroCalendarBot/bot_support.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+def load_env_vars() -> None:
+    script_dir = Path(__file__).resolve().parent
+    env_path = script_dir / '.env'
+    load_dotenv(dotenv_path=env_path)
+
+def get_bot_token() -> str:
+    return os.getenv("BOT_TOKEN")
+
+def get_group_id() -> int:
+    return int(os.getenv("GROUP_ID"))
