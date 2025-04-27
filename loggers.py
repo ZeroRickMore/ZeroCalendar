cc36a6432bf845397003816b63343d80d615a3c0
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-04-27 14:50:33 +0200

    Improve loggers format and differentiate loggers

--- loggers.py
+++ loggers.py
@@ -5,22 +5,33 @@ from flask import request
 #              LOGGERS
 # ====================================
 
-class RequestFormatter(logging.Formatter):
+class MultiFormatter(logging.Formatter):
     def format(self, record):
+        # Default values if not inside a request
         record.remote_addr = request.remote_addr if request else 'N/A'
         record.method = request.method if request else 'N/A'
         record.path = request.path if request else 'N/A'
+
+        # Different format based on logger name
+        if record.name == 'db':
+            self._style._fmt = '%(asctime)s [DB] [%(levelname)s] ===> %(message)s'
+        elif record.name == 'telegrambot':
+            self._style._fmt = '%(asctime)s [TELEGRAM BOT] [%(levelname)s] ===> %(message)s'
+        elif record.name == 'scheduler':
+            self._style._fmt = '%(asctime)s [SCHEDULER] [%(levelname)s] ===> %(message)s'
+        elif record.name == 'werkzeug':
+            self._style._fmt = '%(asctime)s [FLASK] [%(levelname)s] - %(remote_addr)s - %(method)s %(path)s ===> %(message)s'
+
         return super().format(record)
 
+
+
 if __name__ != '__main__':
-    formatter = RequestFormatter(
-        '%(asctime)s - %(levelname)s - %(remote_addr)s - %(method)s %(path)s - %(message)s'
-    )
+    formatter = MultiFormatter()
 
     file_handler = logging.FileHandler('ZeroCalendar.log', encoding='utf-8')
     file_handler.setFormatter(formatter)
 
-    # Flask has too many info logs, I don't want that many...
     werkzeug_logger = logging.getLogger('werkzeug')
     werkzeug_logger.setLevel(logging.WARNING)
     werkzeug_logger.addHandler(file_handler)
@@ -32,3 +43,7 @@ if __name__ != '__main__':
     telegrambot_logger = logging.getLogger('telegrambot')
     telegrambot_logger.setLevel(logging.INFO)
     telegrambot_logger.addHandler(file_handler)
+
+    scheduler_logger = logging.getLogger('scheduler')
+    scheduler_logger.setLevel(logging.INFO)
+    scheduler_logger.addHandler(file_handler)
